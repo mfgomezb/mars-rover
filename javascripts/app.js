9a4f7dd5bb000ef7fdfeb6078f82ca3ce87619ba
mfgomezb <mgomezb89@gmail.com>
2018-08-17 18:34:45 +0200

    Fixed minor errors and modified structure
    Fixed the grid limits (min, max) and the moved from if else statements to switch

--- javascripts/app.js
+++ javascripts/app.js
@@ -10,99 +10,106 @@ var grid = {
   xMin: 0,
   yMin: 0,
   yMax: 10,
-  yMin: 10,
+  xMax: 10,
 }
 
 function turnLeft(rover){
-  console.log("turnLeft was called!");
-  if (rover.direction == 'N'){
-    rover.direction = 'W';
+  console.log('turnLeft was called!"');
+  switch(rover.direction){
+    case "N": rover.direction = 'W';
     console.log("Facing W");
-  } else if (rover.direction == 'W') {
-    rover.direction = 'S';
+    break;
+    case "W": rover.direction = 'S';
     console.log("Facing S");
-  } else if (rover.direction == 'S') {
-    rover.direction = 'E';
+    break;
+    case "S": rover.direction = 'E';
     console.log("Facing E");
-  } else if  (rover.direction == 'E'){
-    rover.direction = 'N';
+    break;
+    case "E": rover.direction = 'N';
     console.log("Facing N");
+    break;
   }
 }
 
 function turnRight(rover){
-  console.log("turnRight was called!");
-  if (rover.direction == 'N'){
-    rover.direction = 'E';
+  console.log('turnLeft was called!"');
+  switch(rover.direction){
+    case "N": rover.direction = 'E';
     console.log("Facing E");
-  } else if (rover.direction == 'E') {
-    rover.direction = 'S';
+    break;
+    case "E": rover.direction = 'S';
     console.log("Facing S");
-  } else if (rover.direction == 'S') {
-    rover.direction = 'W';
+    break;
+    case "S": rover.direction = 'W';
     console.log("Facing W");
-  } else if  (rover.direction == 'W'){
-    rover.direction = 'N';
+    break;
+    case "W": rover.direction = 'N';
     console.log("Facing N");
+    break;
   }
 }
 
-function moveForward(rover){
+function moveForward(rover, grid){
   console.log("moveForward was called")
-  if (rover.direction == 'N'){
-    if (rover.y == grid.yMin){
-      console.log("Off Boundaries")
-    } else {
-      rover.y -= 1;
+  switch (rover.direction){
+    case "N":
+      if (rover.y === grid.yMin) {
+        console.log("Off Boundaries")
+      } else {
+        rover.y--;
+        rover.travelLog.push("X: " +rover.x+ ", Y: " +rover.y);
+        console.log(rover.x + "," + rover.y)
+      }
+      break;
+    case "S":
+      if (rover.y === grid.yMax){
+        console.log("Off Boundaries")
+      } else {
+        rover.y++;
+        rover.travelLog.push("X: " +rover.x+ ", Y: " +rover.y);
+        console.log(rover.x + "," + rover.y)
+      }
+      break;
+    case "W":
+      if (rover.x === grid.xMax){
+        console.log("Off Boundaries")
+      } else {
+        rover.x++;
+        rover.travelLog.push("X: " +rover.x+ ", Y: " +rover.y);
+        console.log(rover.x + "," + rover.y)
+      }
+      break;
+    case "E":
+      if (rover.x === grid.xMin){
+        console.log("Off Boundaries")
+      } else {
+      rover.x--; 
       rover.travelLog.push("X: " +rover.x+ ", Y: " +rover.y);
       console.log(rover.x + "," + rover.y)
-    }
-  
-  } else if (rover.direction == 'S'){
-    if (rover.y == grid.yMax){
-      console.log("Off Boundaries")
-    } else {
-      rover.y += 1;
-      rover.travelLog.push("X: " +rover.x+ ", Y: " +rover.y);
-      console.log(rover.x + "," + rover.y)
-    }
-  
-  } else if (rover.direction == 'W'){
-    if (rover.x == grid.xMax){
-      console.log("Off Boundaries")
-    } else {
-      rover.x += 1;
-      rover.travelLog.push("X: " +rover.x+ ", Y: " +rover.y);
-      console.log(rover.x + "," + rover.y)
-    }
-  
-  } else if (rover.direction == 'E'){
-    if (rover.x == grid.xMin){
-      console.log("Off Boundaries")
-    } else {
-    rover.x -= 1; 
-    rover.travelLog.push("X: " +rover.x+ ", Y: " +rover.y);
-    console.log(rover.x + "," + rover.y)
-    }
+      }
+      break;
   }
 }
 
-input = prompt("Please enter directions '\F' '\R' '\L'")
-directions = input.split("")
-
-function navigate(directions, rover) {
+function navigate(directions, rover, grid) {
   console.log(directions)
   for (i = 0; i < directions.length; i++) {
-    if( directions[i] == 'F' ) {
-      moveForward(rover)
-    } else if ( directions[i] == 'R' ) {
-      turnRight(rover)
-    } else if ( directions[i] == 'L' ) {
-      turnLeft(rover)
-    } else {
-      console.log("Incorrect navigation instructions")
-    }
+    switch (directions.charAt(i)) {
+      case "F":
+        moveForward(rover, grid)
+        break;
+      case "R":
+        turnRight(rover)
+        break;
+      case "L":
+        turnRight(rover)
+        break;
+      default:
+        alert("Please enter a valid command: F, R, L ")
+        break;
+    } 
   }
-  return;
+return; 
 }
 
+directions = prompt("Please enter directions '\F' '\R' '\L'")
